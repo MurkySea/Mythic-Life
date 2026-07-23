329006af4723fcdfe50a11b6088917416c2cf670
MurkySea <markzito825@gmail.com>
2026-07-22 19:12:53 -0500

    Assemble Seraphine chibi from parts A+B+C

---
+++ lib/avatarData/seraphineChibi.ts
@@ -0,0 +1,7 @@
+/** Locked Seraphine chibi icon — assembled from parts */
+import { SERAPHINE_CHIBI_A } from './seraphine_chibi_a'
+import { SERAPHINE_CHIBI_B } from './seraphine_chibi_b'
+import { SERAPHINE_CHIBI_C } from './seraphine_chibi_c'
+
+export const SERAPHINE_CHIBI_JPEG_BASE64 =
+  SERAPHINE_CHIBI_A + SERAPHINE_CHIBI_B + SERAPHINE_CHIBI_C
